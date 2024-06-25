762407be18f916ce7df478211a74f1ceeb8735d8
Tiikss <trakocevic7@gmail.com>
2024-06-25 16:04:54 +0200

    Profile page

---
+++ Projekat/frontend/src/pages/Profile.jsx
@@ -0,0 +1,33 @@
+import React from "react"
+import "../styles/profile.css"
+import slike_profile1 from "../images/profileicon.png"
+import slike_profile2 from "../images/statistika.png"
+
+
+const Profile = () => {
+    return (
+        <div id="profile-container">
+            <div id="profile-main">
+                <div id="profile-info">
+                    <img src={slike_profile1} alt="Profile pic" />
+                    <p id="profile-username"><span>__tiks</span></p>
+                    <p id="profile-name">Ime: <span>Tijana</span></p>
+                    <p id="profile-surname">Prezime: <span>Rakočević</span></p>
+                </div>
+
+                <div id="profile-statistic">
+                    <div id="profile-statistic-title">
+                        <img src={slike_profile2} alt="Statistika" />
+                        <h3 id="profile-h3">Statistika</h3>
+                    </div>
+                    <p id="profile-p">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam alias beatae quisquam quo vitae
+                        doloremque, dignissimos libero rem quis voluptatum accusamus, aut repellat reiciendis delectus
+                        totam.
+                        Inventore sint dolorum nam!</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
