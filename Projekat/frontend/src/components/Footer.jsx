6b4968efb30716aa8dc20fa10bcac12fb9078dc5
Tiikss <trakocevic7@gmail.com>
2024-06-25 15:25:35 +0200

    Footer component

--- Projekat/frontend/src/components/Footer.jsx
+++ Projekat/frontend/src/components/Footer.jsx
@@ -1,9 +1,20 @@
 import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faInstagram, faFacebook } from "@fortawesome/free-brands-svg-icons"
+import "../styles/footer.css"
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div id="footer-container">
+            <footer>
+                <div id="footer-icn-div">
+                    <FontAwesomeIcon icon={faInstagram} className="footer-icn" />
+                    <p className="footer-p">@lingua_franca</p>
+                    <FontAwesomeIcon icon={faFacebook} className="footer-icn" />
+                    <p className="footer-p">Lingua Franca</p>
+                </div>
+                <p className="footer-p">&copy; 2024 Lingua Franca | Sva prava zadržana | Tix</p>
+            </footer>
         </div>
     )
 }
