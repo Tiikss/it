5dcc1dbbaa78b360254e05acf86875207eba2d89
Tiikss <trakocevic7@gmail.com>
2024-06-26 10:46:39 +0200

    Dodavanje dugmeta za odjavljivanje

--- Projekat/frontend/src/pages/Profile.jsx
+++ Projekat/frontend/src/pages/Profile.jsx
@@ -2,9 +2,13 @@ import React from "react"
 import "../styles/profile.css"
 import slike_profile1 from "../images/profileicon.png"
 import slike_profile2 from "../images/statistika.png"
+import { useNavigate } from "react-router-dom"
+
 
 
 const Profile = () => {
+    const navigate = useNavigate();
+
     return (
         <div id="profile-container">
             <div id="profile-main">
@@ -13,6 +17,7 @@ const Profile = () => {
                     <p id="profile-username"><span>__tiks</span></p>
                     <p id="profile-name">Ime: <span>Tijana</span></p>
                     <p id="profile-surname">Prezime: <span>Rakočević</span></p>
+                    <button id="profile-btn" onClick={() => navigate(`/login`)}>Odjavi se</button>
                 </div>
 
                 <div id="profile-statistic">
